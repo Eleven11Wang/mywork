74e92cb9bbf64f82d5556ffc301457dc7065caa0
kkwang <janewang4332@gmail.com>
2018-02-12 23:38:42 +0800

    add seperate by age/sex function

--- brainspanwork.py
+++ brainspanwork.py
@@ -10,7 +10,7 @@ from collections import Counter
 import matplotlib.pyplot as plt
 import os
 
-filepath='/Users/kkwang/mywork/genes_matrix_csv'
+filepath='/Users/kkwang/mywork/gene_array_matrix_csv'
 class brainspanwork(object): 
     
     def __init__(self,filepath=filepath):
@@ -22,29 +22,38 @@ class brainspanwork(object):
         columns_metadata=pd.read_csv('{0}/columns_metadata.csv'.format(self.filepath),index_col=0)
         return columns_metadata
     
-    def get_tissue_names(self):
-        """get sample tissue
-           return a list with tissue name in str
-           len=all columns
-        """
-        filee=self.import_colums_metadata()
-        tissue_names=filee.loc[:,'structure_acronym'].tolist()
-        return tissue_names
-    
+      
     def import_row_metadata(self):
 
         row_metadata=pd.read_csv('{0}/rows_metadata.csv'.format(filepath),index_col=0)
         return row_metadata
     
     def import_expression_matrix(self):
-        expression_matrix=pd.read_csv('{0}/expression_matrix.csv'.format(filepath),index_col=0,header=None)
-        expression_matrix=pd.DataFrame(expression_matrix,index=self.import_row_metadata().index.tolist())
+        expression_matrix_col=self.import_colums_metadata().index.tolist()
+        expression_matrix=pd.read_csv('{0}/expression_matrix.csv'.format(filepath),index_col=0,names=expression_matrix_col)
+        gene_symbol=self.import_row_metadata().loc[:,'gene_symbol'].tolist()
+        expression_matrix['gene_symbol']=gene_symbol
         expression_matrix=expression_matrix.drop_duplicates()
         return expression_matrix
 
+
+class sample_charactor(brainspanwork):
+    def __init__(self):
+        super().__init__()
+    
+    def get_tissue_names(self):
+        """get sample tissue
+           return a list with tissue name in str
+           len=all columns
+        """
+        filee=brainspanwork.import_colums_metadata(self)
+        tissue_names=filee.loc[:,'structure_acronym'].tolist()
+        return tissue_names
+    
     def find_sample_num(self):
         """get the sample number of each tissue return a dict"""
         tissue_names=self.get_tissue_names()
+        
         brain_tissue=set(tissue_names)
         brain_tissue_number={}
         for tissue in brain_tissue:
@@ -52,8 +61,7 @@ class brainspanwork(object):
             brain_tissue_number[tissue]=number
         sorted_brain_tissue_number=sorted(brain_tissue_number.items(),key=lambda x: x[1],reverse=True)
         sorted_brain_tissue_number=dict(sorted_brain_tissue_number)
-        return sorted_brain_tissue_number
-    
+        return sorted_brain_tissue_number 
     def filter_rare_tissue(self,sample_number,tissue_names,expression_matrix):
         """filter tissue that have less then 5 sample 
            return a matrix  
@@ -63,6 +71,7 @@ class brainspanwork(object):
         expression_matrix.drop(columns=extract_list,inplace=True)
         return expression_matrix
     
+
     def find_columns_tissue(self,sample_number,tissue_names):
         """find which columns is which tissue 
 
@@ -79,26 +88,50 @@ class brainspanwork(object):
                 print('something is wrong')
         return fields
 
+    def seperate_by_sex(self):
+        column_data=brainspanwork.import_colums_metadata(self)
+        sex_dic={'female':[],'male':[]}
+        for i,p in enumerate(column_data.loc[:,'gender']):
+            if p == 'M':
+                sex_dic['male'].append(i+1)
+            else:
+                sex_dic['female'].append(i+1)
+        return sex_dic
+    def seperate_by_time(self):
+        column_data=brainspanwork.import_colums_metadata(self)
+        age_dic={'prenatal':['8 pcw','9 pcw','12 pcw','13 pcw','16 pcw','17 pcw','19 pcw','21 pcw','24 pcw','25 pcw','26 pcw'],'early child':['4 mos','10 mos','1 yrs','2 yrs','3 yrs','4 yrs','8 yrs'],'puberty':['13 yrs','15 yrs','18 yrs'],'adult':['21 yrs','23 yrs','30 yrs','36 yrs','37 yrs','40 yrs']}
+        time_dic={'prenatal':[],'early child':[],'puberty':[],'adult':[]}
 
+        column_data_age=column_data.loc[:,'age'].tolist()
+        
+        for i,age in enumerate(column_data_age):
+            for time in time_dic.keys():
+                if age in age_dic[time]:
+                    time_dic[time].append(i+1)
+        return time_dic
+
+        
 
 def main():
     work=brainspanwork()
     expression_matrix=work.import_expression_matrix()
-    tissue_names=work.get_tissue_names() # return a list of full columns lens which tissue it is 
-    sample_number=work.find_sample_num() # return a sorted tissue and number of it type:dict
+    
+    sample=sample_charactor()
+    tissue_names=sample.get_tissue_names() # return a list of full columns lens which tissue it is 
+    sample_number=sample.find_sample_num() # return a sorted tissue and number of it type:dict
 
     filter_by_five=False #<-- change whether you what to filter rare tissue 
 
     if filter_by_five is True:
-        
-        filted_expression_matrix=work.filter_rare_tissue(sample_number,tissue_names,expression_matrix) 
+        filted_expression_matrix=sample.filter_rare_tissue(sample_number,tissue_names,expression_matrix) 
         #filter the tissue that less then five 
         sample_number={key:value for key,value in sample_number.items() if value > 5}
     
-    columns_tissue=work.find_columns_tissue(sample_number,tissue_names) #find which cloumns is which tissue (
+    columns_tissue=sample.find_columns_tissue(sample_number,tissue_names) #find which cloumns is which tissue (
+    sexual_data=sample.seperate_by_sex()
    
-    print(columns_tissue)
-    print(expression_matrix.index)
-    print(expression_matrix.columns)
+    print(sexual_data)
+    time_data=sample.seperate_by_time()
+    print(time_data)
 if __name__ == "__main__":
     main()
--- readme.txt
+++ readme.txt
@@ -1,14 +1,7 @@
-Git is a distributed version control system 
-Git is free software distributed under the GPL
-Git has a mutanle index called stage 
-Git tracks changes
-hhh
-Creating a new branch in quick
-add a newpy file which is a bash script for python script
-add a file call expression matrix
-
-    filter the tissue that less then five (filter_rare_tissue)
-    find which column is which tissue 
-    seperate the matrix by age 
-    seperate the matrix by sex
+class brainspanwork 
+    class sample_charactor(brainspanwork)
+        function : filter the tissue that less then five 
+        function : find which columns is which tissue 
+        function : seperate the matrix by age 
+        seperate : the matrix by sex
 
